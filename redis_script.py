8400cbb5dbdaf2617b8bfd8da59be6a9807021cb
devbydaylight <50723211+devbydaylight@users.noreply.github.com>
2021-05-19 14:45:57 -0500

    Update redis_script.py

--- redis_script.py
+++ redis_script.py
@@ -3,7 +3,7 @@ import redis
 redis = redis.Redis(host= '172.31.28.194', port= '6379')
 redis.set('a', 5)
 redis.set('b', 6)
-redis.set('c', a+b)
+redis.call('set','c',redis.call('get','a')+redis.call('get','b')) # can't quite get this to work correctly, no attribute for redis.call. Need to research later.
 value1 = redis.get('a')
 value2 = redis.get('b')
 value3 = redis.get('c')
